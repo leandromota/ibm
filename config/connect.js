af720fbc943544447e16188113b2750491fb5ee5
Leandro mota <leandrosoulmota@gmail.com>
2018-06-03 20:18:42 -0300

    Sequelize Modules

---
+++ config/connect.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('lirollajr53', 'lirollajr53', 'Pw@senh4', {
+    host: '179.188.16.118',
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+
+    operatorsAliases: false
+});
+
+const Books = sequelize.define('ibm_books', {
+    id: {type: Sequelize.INTEGER, primaryKey: true},
+    name: {type: Sequelize.STRING},
+    author: {type: Sequelize.STRING}
+}, {timestamps: false})
+
+const Users = sequelize.define('ibm_users', {
+    id: {type: Sequelize.INTEGER, primaryKey: true},
+    name: {type: Sequelize.STRING}
+}, {timestamps: false})
+
+Books.findAll().then(result => console.log(result));
